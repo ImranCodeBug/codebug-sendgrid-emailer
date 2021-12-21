3968fbb20e5738ef2b621cb3b36064970edb563b
Imran <imran.loves.to.code@gmail.com>
2021-12-21 18:35:06 +0000

    Added syntax highlighter and place holder to hold the email content

---
+++ SendGridEmailer/Components/TemplateContentComponent.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+
+interface Props {
+    
+}
+
+const TemplateContentComponent = (props: Props) => {
+    return (
+        <div> 
+            
+        </div>
+    )
+}
+
+export default TemplateContentComponent
